454e119bca7ad0d17ac946e6624aa2061409020d
hungpd <hungpd@tinhvan.com>
2016-09-25 15:58:22 +0700

    Configuration upload max file size 10MB

--- SibLinksService/src/main/java/com/siblinks/ws/common/DAOException.java
+++ SibLinksService/src/main/java/com/siblinks/ws/common/DAOException.java
@@ -1,20 +1,44 @@
+/*
+ * Copyright (c) 2016-2017, Tinhvan Outsourcing JSC. All rights reserved.
+ *
+ * No permission to use, copy, modify and distribute this software
+ * and its documentation for any purpose is granted.
+ * This software is provided under applicable license agreement only.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.siblinks.ws.common;
 
 import java.io.Serializable;
 
+/**
+ * 
+ * @author hungpd
+ * @version 1.0
+ */
 
-public class DAOException extends BaseException
-{
-	
-	/**
+public class DAOException extends BaseException {
+
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public DAOException(Throwable aThrowable, String userMessageKey, Serializable[] valueReplacementArray, ErrorLevel level)
-	{
-		super(aThrowable, userMessageKey, valueReplacementArray, level);
-	}
-	
-	
+    public DAOException(
+            final Throwable aThrowable,
+            final String userMessageKey,
+            final Serializable[] valueReplacementArray,
+            final ErrorLevel level) {
+        super(aThrowable, userMessageKey, valueReplacementArray, level);
+    }
 }
