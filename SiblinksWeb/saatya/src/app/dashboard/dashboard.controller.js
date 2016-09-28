4b62b20abc1d1b387403f69ac63fffc3de9eefb9
Jackie Nguyen <neverbackdown.1988@gmail.com>
2016-09-28 09:21:35 +0700

    master: - update: play video by using youtube api

--- SiblinksWeb/saatya/src/app/dashboard/dashboard.controller.js
+++ SiblinksWeb/saatya/src/app/dashboard/dashboard.controller.js
@@ -37,31 +37,74 @@ brotControllers.controller('DashboardController',['$scope','$http', 'MentorServi
      VideoService.getVideosTopViewed(userId, 0).then(function(data){
           if (data.data.request_data_result != null && data.data.request_data_result.length > 0) {
             $scope.videosTopViewed = data.data.request_data_result;
-            $scope.vTopViewed = $scope.videosTopViewed[0];
+            $scope.vTopViewed = $scope.videosTopViewed[0];            
             $scope.topViewedPos = 0;
+            initYoutubePlayer($scope.vTopViewed.url)
           }
         });
   }
 
+  function initYoutubePlayer(youtubeUrl){
+    var videoid = youtubeUrl.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+    if (videoid != null) {
+      if (typeof(player) == "undefined") {
+        onYouTubeIframeAPIReady(videoid[1]);
+      } else {
+        player.loadVideoById(videoid[1]);
+      }      
+    }
+  }
+
   $scope.topViewedPre = function(pos){
+    console.log(pos);
     if (pos == 0) {
       $scope.topViewedPos = $scope.videosTopViewed.length - 1;
       $scope.vTopViewed = $scope.videosTopViewed[$scope.videosTopViewed.length - 1];
+      initYoutubePlayer($scope.vTopViewed.url);
     }
     else{
       $scope.topViewedPos = pos - 1;
       $scope.vTopViewed = $scope.videosTopViewed[pos - 1];
+      initYoutubePlayer($scope.vTopViewed.url);
     }
   }
 
   $scope.topViewedNext = function(pos){
+    console.log(pos);
     if (pos == $scope.videosTopViewed.length - 1) {
       $scope.topViewedPos = 0;
       $scope.vTopViewed = $scope.videosTopViewed[0];
+      initYoutubePlayer($scope.vTopViewed.url);
     }
     else{
       $scope.topViewedPos = pos + 1;
       $scope.vTopViewed = $scope.videosTopViewed[pos + 1];
+      initYoutubePlayer($scope.vTopViewed.url);
     }
   }
+
+  function onYouTubeIframeAPIReady(youtubeId) {
+    player = new YT.Player('video', {
+        height: '430',
+        width: '100%',
+        videoId: youtubeId,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        },
+        playerVars: {
+            showinfo: 0,
+            autohide: 1,
+            theme: 'dark'
+        }
+    });
+  }
+
+  function onPlayerReady(event) {
+    // event.target.playVideo();
+  }
+
+  function onPlayerStateChange(event) {
+  }
+
 }]);
