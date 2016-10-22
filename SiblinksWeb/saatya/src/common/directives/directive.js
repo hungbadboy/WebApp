b1c5d72ce7061e8ee63bed35ff37a296d9e0d73f
dam <dam.doan.ts@gmail.com>
2016-10-22 17:23:20 +0700

    fix modal delete

--- SiblinksWeb/saatya/src/common/directives/directive.js
+++ SiblinksWeb/saatya/src/common/directives/directive.js
@@ -1000,8 +1000,8 @@ brotControllers.directive('ngReallyClick', ['$modal',
                     var message = attrs.ngReallyMessage || "Are you sure ?";
 
                     var modalHtml = ' <div class="modal-body">' + message + '</div>';
-                    modalHtml += '<div class="modal-footer"><button class="btn btn-primary" ng-click="ok()">' +
-                        'OK</button><button class="btn btn-warning" ng-click="cancel()">Cancel</button></div>';
+                    modalHtml += '<div class="modal-footer"><button class="btn btn-danger" ng-click="ok()">' +
+                        'Delete</button><button class="btn btn-default" ng-click="cancel()">Cancel</button></div>';
 
                     var modalInstance = $modal.open({
                         template: modalHtml,
@@ -1014,7 +1014,6 @@ brotControllers.directive('ngReallyClick', ['$modal',
                     }, function() {
                         //Modal dismissed
                     });
-                    //*/
 
                 });
 
