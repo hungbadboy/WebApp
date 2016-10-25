fdc227324fc9fed06db1b84acb74d10de7b08809
hungpd <hungpd@tinhvan.com>
2016-10-25 17:55:29 +0700

    Ghep giao dien cho about us

--- SiblinksWeb/saatya/src/common/utils/utils.js
+++ SiblinksWeb/saatya/src/common/utils/utils.js
@@ -403,21 +403,16 @@ function timeBackEnd() {
 }
 
 function resetUI() {
-    clearMessages();
     showToken('loading...');
-    // [START get_token]
-    // Get Instance ID token. Initially this makes a network call, once retrieved
-    // subsequent calls to getToken will return from cache.
     messaging.getToken()
     .then(function(currentToken) {
       if (currentToken) {
         sendTokenToServer(currentToken);
-        updateUIForPushEnabled(currentToken);
+        showToken(currentToken);
       } else {
         // Show permission request.
         console.log('No Instance ID token available. Request permission to generate one.');
         // Show permission UI.
-        //updateUIForPushPermissionRequired();
         requestPermission();
         setTokenSentToServer(false);
       }
@@ -434,25 +429,24 @@ function resetUI() {
     var tokenElement = document.querySelector('#token');
     tokenElement.textContent = currentToken;
   }
-  // Send the Instance ID token your application server, so that it can:
-  // - send messages back to this app
-  // - subscribe/unsubscribe the token from topics
+
   function sendTokenToServer(currentToken) {
     if (!isTokenSentToServer()) {
       console.log('Sending token to server...');
-      // TODO(developer): Send the current token to your server.
       setTokenSentToServer(true);
     } else {
       console.log('Token already sent to server so won\'t send it again ' +
           'unless it changes');
     }
   }
+  
   function isTokenSentToServer() {
     if (window.localStorage.getItem('sentToServer') == 1) {
           return true;
     }
     return false;
   }
+  
   function setTokenSentToServer(sent) {
     if (sent) {
       window.localStorage.setItem('sentToServer', 1);
@@ -460,26 +454,14 @@ function resetUI() {
       window.localStorage.setItem('sentToServer', 0);
     }
   }
-  function showHideDiv(divId, show) {
-    const div = document.querySelector('#' + divId);
-    if (show) {
-      //div.style = "display: visible";
-    } else {
-      div.style = "display: none";
-    }
-  }
+  
   function requestPermission() {
     console.log('Requesting permission...');
     // [START request_permission]
     messaging.requestPermission()
     .then(function() {
       console.log('Notification permission granted.');
-      // TODO(developer): Retrieve a Instance ID token for use with FCM.
-      // [START_EXCLUDE]
-      // In many cases once an app has been granted notification permission, it
-      // should update its UI reflecting this.
       resetUI();
-      // [END_EXCLUDE]
     })
     .catch(function(err) {
       console.log('Unable to get permission to notify. ', err);
@@ -509,31 +491,4 @@ function resetUI() {
       console.log('Error retrieving Instance ID token. ', err);
       showToken('Error retrieving Instance ID token. ', err);
     });
-  }
-  // Add a message to the messages element.
-  function appendMessage(payload) {
-    const messagesElement = document.querySelector('#messages');
-    const dataHeaderELement = document.createElement('h5');
-    const dataElement = document.createElement('pre');
-    dataElement.style = 'overflow-x:hidden;'
-    dataHeaderELement.textContent = 'Received message:';
-    dataElement.textContent = JSON.stringify(payload, null, 2);
-    messagesElement.appendChild(dataHeaderELement);
-    messagesElement.appendChild(dataElement);
-  }
-  // Clear the messages element of all children.
-  function clearMessages() {
-    const messagesElement = document.querySelector('#messages');
-    while (messagesElement.hasChildNodes()) {
-      messagesElement.removeChild(messagesElement.lastChild);
-    }
-  }
-  function updateUIForPushEnabled(currentToken) {
-    showHideDiv(tokenDivId, true);
-    showHideDiv(permissionDivId, false);
-    showToken(currentToken);
-  }
-  function updateUIForPushPermissionRequired() {
-    showHideDiv(tokenDivId, false);
-    showHideDiv(permissionDivId, true);
   }
