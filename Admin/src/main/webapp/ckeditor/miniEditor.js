1013e10ceaf8faa702592b173c0fa35534678c53
hungpd <hungpd@tinhvan.com>
2016-10-28 18:06:37 +0700

    Update CKEditor

--- Admin/src/main/webapp/ckeditor/miniEditor.js
+++ Admin/src/main/webapp/ckeditor/miniEditor.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -10,10 +10,20 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
-		{ name: 'basicstyles'},{ name: 'styles' },
-		{ name:  'list'}, {name:'indent'},
-		{ name:  'redo'}, {name:'undo'},
+//		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
 		{ name: 'insert' },
+//		{ name: 'forms' },
+		{ name: 'tools' },
+//		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		//{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+//		{ name: 'about' }
 	];
 
 	// Remove some buttons provided by the standard plugins, which are
