7953f93f19992b7e7f5d85a7f1cc41219a9eeeb9
Trần Thị Thanh Hồng <hongttt@tinhvan.com>
2016-11-01 16:00:10 +0700

    [U] fix essay

--- SiblinksWeb/saatya/src/app/essay/allessay.controller.js
+++ SiblinksWeb/saatya/src/app/essay/allessay.controller.js
@@ -18,37 +18,40 @@ brotControllers.controller('AllEssayCtrl', ['$scope', '$location', '$window', 'E
   init();
 
   function init(){
-    if (userId && userId > 0) {
+    
+    if (userId && userId > 0) {      
       $(window).scroll(function(){ 
         var qa_scroll = $(window).scrollTop();
+        var heighttab = $(window).height() - 258;
+            heighttab_scroll = $(window).height() - 320;
         if (qa_scroll > 75) {
-          $(".mentor-manage-qa-content .left-qa").css({"top":"108px", "height":"85%"});
-          $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"69vh"});
-        }
-        else {
-          $(".mentor-manage-qa-content .left-qa").css({"top":"auto"});
-          $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"64vh"});
-        }
-        if($window.innerWidth < 1601){
-          if (qa_scroll > 75) {
-            $(".mentor-manage-qa-content .left-qa").css({"top":"108px", "height":"85%"});
-            $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"70vh"});
+          $(".mentor-manage-essay .left-qa").css({"top":"105px"});
+              $(".mentor-manage-essay .left-qa .tab-content .tab-pane").css({"height":+ heighttab + "px"});
           }
           else {
-            $(".mentor-manage-qa-content .left-qa").css({"top":"auto"});
-            $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"63vh"});
-          }
-        }
-        if($window.innerWidth < 1441){
-          if (qa_scroll > 75) {
-            $(".mentor-manage-qa-content .left-qa").css({"top":"108px", "height":"84%"});
-            $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"67vh"});
-          }
-          else {
-            $(".mentor-manage-qa-content .left-qa").css({"top":"auto", "height":"74%"});
-            $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"65vh"});
-          }
-        }
+              $(".mentor-manage-essay .left-qa").css({"top":"auto"});
+              $(".mentor-manage-essay .left-qa .tab-content .tab-pane").css({"height":+ heighttab_scroll + "px"});
+          } 
+        // if($window.innerWidth < 1601){
+        //   if (qa_scroll > 75) {
+        //     $(".mentor-manage-qa-content .left-qa").css({"top":"108px", "height":"85%"});
+        //     $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"70vh"});
+        //   }
+        //   else {
+        //     $(".mentor-manage-qa-content .left-qa").css({"top":"auto"});
+        //     $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"63vh"});
+        //   }
+        // }
+        // if($window.innerWidth < 1441){
+        //   if (qa_scroll > 75) {
+        //     $(".mentor-manage-qa-content .left-qa").css({"top":"108px", "height":"84%"});
+        //     $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"67vh"});
+        //   }
+        //   else {
+        //     $(".mentor-manage-qa-content .left-qa").css({"top":"auto", "height":"74%"});
+        //     $(".mentor-manage-qa-content .left-qa .tab-content").css({"height":"65vh"});
+        //   }
+        // }
       })
       getAllEssay();
     } else {
