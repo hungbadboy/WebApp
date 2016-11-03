a1367df1418a24f85052612f32bb1c5ceb490414
hungpd <hungpd@tinhvan.com>
2016-11-03 17:38:01 +0700

    Add duration playlist detail and video detail for student

--- SiblinksWeb/saatya/src/app/topMentor/topMentors.controller.js
+++ SiblinksWeb/saatya/src/app/topMentor/topMentors.controller.js
@@ -3,33 +3,23 @@ brotControllers.controller('TopMentorCtrl', ['$scope', '$http', 'MentorService',
 	var OFFSET = 0;
 	var userId = localStorage.getItem('userId') || '-1';
 	var subjects = JSON.parse(localStorage.getItem('subjects'));
+	$scope.listTopmentors = [];
+	
 	init();
 	function init() {
         //get top mentors by subcribe
 	    MentorService.getTopMentorsByLikeRateSubcrible(LIMIT_TOP_MENTORS, OFFSET, 'subcribe', userId).then(function (data) {
 	        var data_result = data.data.request_data_result;
 	        if (data.data.status == 'true') {
-	            var listTopMentors = [];
 	            for (var i = 0; i < data_result.length; i++) {
-	                var mentor = {};
-	                mentor.userid = data_result[i].userid;
-	                mentor.fullName = displayUserName(mentor.firstName = data_result[i].firstName,
-	                			mentor.lastName = data_result[i].lastName,
-	                			data_result[i].loginName);
-	                mentor.imageUrl = data_result[i].imageUrl;
-	                mentor.numlike = data_result[i].numlike;
-	                mentor.numsub = data_result[i].numsub;
-	                mentor.numvideos = data_result[i].numvideos;
-	                mentor.isOnline = data_result[i].isOnline;
-	                mentor.defaultSubjectId = data_result[i].defaultSubjectId;
-	                if(data_result[i].defaultSubjectId !== null && data_result[i].defaultSubjectId !== undefined) {
-	                    mentor.listSubject = getSubjectNameById(data_result[i].defaultSubjectId, subjects);
+	            	if(data_result[i].defaultSubjectId !== null && data_result[i].defaultSubjectId !== undefined) {
+	            		data_result[i].listSubject = getSubjectNameById(data_result[i].defaultSubjectId, subjects);
 	                }
-	                mentor.numAnswers = data_result[i].numAnswers;
-	                listTopMentors.push(mentor);
+	            	$scope.listTopmentors.push(data_result[i]);
 	            }
+	        } else {
+	        	$scope.listTopmentors ={};
 	        }
-	        $scope.listTopmentors = listTopMentors;
 	    });
 	}
 }]);
